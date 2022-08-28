32574d146cddcee5aa8a71af7cc5cdc8f09e8ee9
moli0805 <amolsharma480@gmail.com>
2022-08-28 17:45:52 +0530

    inital commit

---
+++ src/app/core/numberWith2Decimal/number-with2-decimal.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NumberWith2DecimalDirective } from './number-with2-decimal.directive';
+
+describe('NumberWith2DecimalDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NumberWith2DecimalDirective();
+    expect(directive).toBeTruthy();
+  });
+});
