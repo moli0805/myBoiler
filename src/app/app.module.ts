9ca4d31c1512f20ae7dd5955126c4369888fb153
moli0805 <amolsharma480@gmail.com>
2022-08-28 17:52:06 +0530

    number with 2 decimal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NumberWith2DecimalDirective } from './core/numberWith2Decimal/number-with2-decimal.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberWith2DecimalDirective
   ],
   imports: [
     BrowserModule,
